d30e83404729906938c7e0b6901847865d503621
Juan <jjoa@intenllisys.com.do>
2020-03-23 11:28:02 -0400

    Vue training

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,27 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import EventList from "../views/EventList.vue";
+import EventCreate from "../views/EventCreate.vue"
+import EventShow from "../views/EventShow.vue"
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "event-list",
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/event/:id",
+    name: "event-show",
+    component: EventShow,
+    props: true
+  },
+  {
+    path: "/event/create-event",
+    name: "event-create",
+    component: EventCreate
   }
 ];
 
@@ -28,3 +32,16 @@ const router = new VueRouter({
 });
 
 export default router;
+
+//To direct page with alias
+    // alias: "/about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    // component: () =>
+    //   import(/* webpackChunkName: "about" */ "../views/About.vue")
+//To redirect page
+  // {
+  //   path: "/about",
+  //   redirect: { name: "About" }
+  // }
